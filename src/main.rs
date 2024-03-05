1562f7e9e4539bf5b5a6adc5646da90a2d9fceff
kayushkin1click <vkayushkin@1clicklogistics.com>
2024-03-05 09:27:43 -0800

    added timing and fixed rendering

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use crossterm::event::{read, Event, KeyCode};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use rand::{thread_rng, Rng};
+use rand::{seq::SliceRandom, thread_rng};
 use std::io::{self, Write};
+use std::time::Instant;
 
 fn main() {
     let mut words: Vec<&str> = [
@@ -35,21 +36,24 @@ fn main() {
         "blackberry",
     ]
     .to_vec();
-    let correct_words = 5; //words.len();
+    let correct_words = 18; //words.len();
+    let mut words_per_line = 10;
     let mut correct = 0;
     let mut words_typed = 0;
     //randomly sort the words
-    words.sort_by(|_, _| thread_rng().gen::<u8>().cmp(&0));
+    let mut rng = thread_rng();
+    let mut continue_word = false;
+    words.shuffle(&mut rng);
 
-    let mut words_per_line = 5;
+    let start = Instant::now();
     while words_typed < correct_words {
         if words_per_line + words_typed > correct_words {
             words_per_line = correct_words - words_typed;
         }
         let current_words = words[words_typed..words_typed + words_per_line].join(" ");
         println!("{}", current_words);
-        let mut current_word = String::new();
         let _ = enable_raw_mode();
+        let mut current_word = String::new();
         loop {
             match read() {
                 Ok(Event::Key(event)) => match event.code {
@@ -59,6 +63,7 @@ fn main() {
                             correct += 1;
                         }
                         words_typed += 1;
+                        continue_word = true;
                         current_word.clear();
                         if words_typed == correct_words {
                             break;
@@ -73,11 +78,24 @@ fn main() {
                 },
                 _ => {}
             }
-            if words_typed == correct_words {
+            if continue_word && words_typed % words_per_line == 0 {
+                continue_word = false;
                 break;
             }
         }
         let _ = disable_raw_mode();
+        println!("");
+        if words_typed >= correct_words {
+            let elapsed = start.elapsed();
+            println!("Time: {:?}", elapsed);
+            println!("Words typed: {}", words_typed);
+            println!("time secs: {}", elapsed.as_secs_f64() as f64);
+            println!(
+                "{}: wpm",
+                correct as f64 / (elapsed.as_secs_f64() / 60.0) as f64
+            );
+            break;
+        }
     }
 
     println!("You got {} out of {} correct!", correct, correct_words);
