d77e77a76a236b922c2a80d174505a40c7202d82
kayushkin1click <vkayushkin@1clicklogistics.com>
2024-03-08 09:45:45 -0800

    fixed double printing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::{self, Write};
 use std::time::Instant;
 
 fn main() {
+    // big list of random words
     let mut words: Vec<&str> = [
         "cat",
         "Gelatin",
@@ -34,9 +35,33 @@ fn main() {
         "watermelon",
         "avocado",
         "blackberry",
+        "blueberry",
+        "Artichoke",
+        "Asparagus",
+        "Beet",
+        "Broccoli",
+        "Cabbage",
+        "Carrot",
+        "Cauliflower",
+        "Celery",
+        "Corn",
+        "Cucumber",
+        "Eggplant",
+        "car",
+        "truck",
+        "bus",
+        "Asphalt",
+        "Bicycle",
+        "warehouse",
+        "Cement",
+        "yellow",
+        "Daisy",
+        "Dandelion",
+        "Daffodil",
     ]
     .to_vec();
-    let correct_words = 18; //words.len();
+
+    let total_words = 18; //words.len();
     let mut words_per_line = 10;
     let mut correct = 0;
     let mut words_typed = 0;
@@ -46,11 +71,15 @@ fn main() {
     words.shuffle(&mut rng);
 
     let start = Instant::now();
-    while words_typed < correct_words {
-        if words_per_line + words_typed > correct_words {
-            words_per_line = correct_words - words_typed;
+    while words_typed < total_words {
+        if words_per_line + words_typed > total_words {
+            words_per_line = total_words - words_typed;
         }
-        let current_words = words[words_typed..words_typed + words_per_line].join(" ");
+        let mut end_word = words_typed + words_per_line;
+        if words_typed + words_per_line > total_words {
+            end_word = total_words;
+        }
+        let current_words = words[words_typed..end_word].join(" ");
         println!("{}", current_words);
         let _ = enable_raw_mode();
         let mut current_word = String::new();
@@ -65,7 +94,7 @@ fn main() {
                         words_typed += 1;
                         continue_word = true;
                         current_word.clear();
-                        if words_typed == correct_words {
+                        if words_typed == total_words {
                             break;
                         }
                     }
@@ -78,14 +107,16 @@ fn main() {
                 },
                 _ => {}
             }
-            if continue_word && words_typed % words_per_line == 0 {
+            if continue_word && words_typed % 10 % words_per_line == 0 || words_typed >= total_words
+            {
                 continue_word = false;
                 break;
             }
         }
         let _ = disable_raw_mode();
         println!("");
-        if words_typed >= correct_words {
+
+        if words_typed >= total_words {
             let elapsed = start.elapsed();
             println!("Time: {:?}", elapsed);
             println!("Words typed: {}", words_typed);
@@ -98,5 +129,5 @@ fn main() {
         }
     }
 
-    println!("You got {} out of {} correct!", correct, correct_words);
+    println!("You got {} out of {} correct!", correct, total_words);
 }
